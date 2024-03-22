da26075ce3b431bce0cb5d09158fb2af507acb82
Yannick M <85898240+mugishayannick@users.noreply.github.com>
2024-03-22 09:26:54 +0200

    Fixed the landing page

---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+import { Grid1, Grid3, Grid2 } from "./images";
+
+
+export const HeaderLists = [
+    {
+        id: 1,
+        title: "Home"
+    },
+    {
+        id: 2,
+        title: "Features"
+    },
+    {
+        id: 3,
+        title: "About"
+    },
+    {
+        id: 4,
+        title: "Help Center & Community"
+    }
+]
+
+export const GridData = [
+    {
+        image: Grid1,
+        title: '1. Click Capture',
+        description: 'Once you install the Chrome extension, fire it up, and click “Capture”.'
+    },
+    {
+        image: Grid2,
+        title: '2. Perform Your Actions',
+        description: 'Once capturing, Wizardshot carefully monitors what you do to understand how to document it.'
+    },
+    {
+        image: Grid3,
+        title: '1. ✅ Done.Article Written',
+        description: 'Wizardshot carefully crafts the perfect article based off your actions recorded. Just click “Done” and we’ll write up the article in a second.'
+    }
+]
